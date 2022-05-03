1191c7c89980c954ab983c01605160394ff02885
mjschwenne <mjschwenne@gmail.com>
2022-05-03 11:43:30 -0400

    init commit with fish alias

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# alias for dotfile tracking
+alias config="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
