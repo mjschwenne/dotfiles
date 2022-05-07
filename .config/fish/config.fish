0db38708d13b32f2045143e8c0892ced6a9828f5
mjschwenne <mjschwenne@gmail.com>
2022-05-06 20:30:42 -0400

    bar config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,10 @@ end
 
 # alias for dotfile tracking
 alias config="git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
+
+# start the ssh-agent
+if test -z (pgrep ssh-agent)
+	eval (ssh-agent -c)
+	set -Ux SSH_AUTH_SOCK $SSH_AUTH_SOCK
+  	set -Ux SSH_AGENT_PID $SSH_AGENT_PID
+end
--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -27,33 +27,22 @@
 from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
-from libqtile.utils import guess_terminal
 import subprocess
 import os
+from copy import deepcopy
 from libqtile import hook
 
-@hook.subscribe.startup
-def dbus_register():
-    id = os.environ.get('DESKTOP_AUTOSTART_ID')
-    if not id:
-        return
-    subprocess.Popen(['dbus-send',
-                      '--session',
-                      '--print-reply',
-                      '--dest=org.gnome.SessionManager',
-                      '/org/gnome/SessionManager',
-                      'org.gnome.SessionManager.RegisterClient',
-                      'string:qtile',
-                      'string:' + id])
+terminal = "kitty"
 
+# AUTOSTART
 @hook.subscribe.startup_once
 def autostart():
-    home = os.path.expanduser('~/.config/qtile/autostart.sh')
+    home = os.path.expanduser("~/.config/qtile/autostart.sh")
     subprocess.run([home])
 
 
-mod = "mod4"
-terminal = "kitty"
+# KEY BINDINGS
+mod = "mod4"  # "Windows" key
 
 keys = [
     # A list of available commands that can be bound to keys can be found
@@ -63,19 +52,27 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other windw"),
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
+    # Move windows
+    Key(
+        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
+    ),
+    Key(
+        [mod, "shift"],
+        "l",
+        lazy.layout.shuffle_right(),
+        desc="Move window to the right",
+    ),
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
+    # Resize Windows
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+    Key(
+        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
+    ),
     Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+    # Reset window size
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
@@ -87,33 +84,45 @@ keys = [
         lazy.layout.toggle_split(),
         desc="Toggle between split and unsplit sides of stack",
     ),
+    # Terminal
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+    # Browser
+    Key([mod], "b", lazy.spawn("brave-browser"), desc="Launch browser"),
+    # Application switcher
+    Key([mod], "space", lazy.spawn("rofi -show"), desc="Launch Application Switcher"),
+    # Application launcher
+    Key([mod], "r", lazy.spawn("rofi -show run"), desc="Launch Application Launcher"),
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    # Close current window
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
+    # Lock computer
+    Key([mod], "l", lazy.spawn("dm-tool lock"), desc="Lock the Computer"),
+    # Qtile controls
     Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
     Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
 ]
 
-groups = [Group(i) for i in "123456789"]
+# WORKSPACES
+groups = [Group(i) for i in [" ", " ", " ", " ", " ", " ", " ", " "]]
+group_hotkeys = "asdfuiop"
 
-for i in groups:
+for g, k in zip(groups, group_hotkeys):
     keys.extend(
         [
             # mod1 + letter of group = switch to group
             Key(
                 [mod],
-                i.name,
-                lazy.group[i.name].toscreen(),
-                desc="Switch to group {}".format(i.name),
+                k,
+                lazy.group[g.name].toscreen(),
+                desc=f"Switch to group {i.name}",
             ),
             # mod1 + shift + letter of group = switch to & move focused window to group
             Key(
                 [mod, "shift"],
-                i.name,
-                lazy.window.togroup(i.name, switch_group=True),
-                desc="Switch to & move focused window to group {}".format(i.name),
+                k,
+                lazy.window.togroup(g.name, switch_group=True),
+                desc=f"Switch to & move focused window to group {i.name}",
             ),
             # Or, use below if you prefer not to switch to that group.
             # # mod1 + shift + letter of group = move focused window to group
@@ -122,62 +131,229 @@ for i in groups:
         ]
     )
 
+# LAYOUTS
+
+# First, some color
+catppuccin = {
+    "flamingo": "#F2CDCD",
+    "mauve": "#DDB6F2",
+    "pink": "#F5C2E7",
+    "maroon": "#E8A2AF",
+    "red": "#F28FAD",
+    "peach": "#F8BD96",
+    "yellow": "#FAE3B0",
+    "green": "#ABE9B3",
+    "teal": "#B5E8E0",
+    "blue": "92CDFB",
+    "sky": "#89DCEB",
+    "white": "#D9E0EE",
+    "gray0": "#6E6C7E",
+    "black1": "#1A1826",
+}
+
+default_layout_config = {
+    "border_focus": catppuccin["green"],
+    "border_normal": catppuccin["red"],
+    "border_width": 2,
+    "margin": 3,
+}
+
 layouts = [
-    layout.Columns(border_focus_stack=["#d75f5f", "#8f3d3d"], border_width=4),
+    layout.Columns(**default_layout_config),
     layout.Max(),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
     # layout.Bsp(),
     # layout.Matrix(),
-    # layout.MonadTall(),
+    layout.MonadTall(**default_layout_config),
     # layout.MonadWide(),
-    # layout.RatioTile(),
+    layout.RatioTile(**default_layout_config),
     # layout.Tile(),
     # layout.TreeTab(),
     # layout.VerticalTile(),
     # layout.Zoomy(),
 ]
 
+# PANEL
+
+
 widget_defaults = dict(
-    font="sans",
-    fontsize=12,
+    font="JetBrainsMono Nerd Font",
+    fontsize=16,
     padding=3,
+    foreground=catppuccin["black1"],
 )
 extension_defaults = widget_defaults.copy()
 
+
+def get_widgets(primary=False):
+    widgets = [
+        widget.Spacer(length=3, background="#00000000"),
+        widget.TextBox(
+            text="",
+            padding=0,
+            fontsize=30,
+            foreground=catppuccin["pink"],
+            background="#00000000",
+        ),
+        widget.TextBox(
+            text="異",
+            mouse_callbacks={"Button1": lazy.spawn("rofi -show run")},
+            background=catppuccin["pink"],
+        ),
+        widget.TextBox(
+            text="",
+            padding=0,
+            fontsize=30,
+            foreground=catppuccin["pink"],
+            background=catppuccin["red"],
+        ),
+        widget.CurrentLayoutIcon(
+            padding=1,
+            scale=0.8,
+            background=catppuccin["red"],
+            custom_icon_paths=["/home/mjs/.config/qtile/icons/"],
+        ),
+        widget.CurrentLayout(background=catppuccin["red"]),
+        widget.TextBox(
+            text="",
+            padding=0,
+            fontsize=30,
+            foreground=catppuccin["red"],
+            background=catppuccin["peach"],
+        ),
+        widget.GroupBox(
+            highlight_method="line",
+            background=catppuccin["peach"],
+            highlight_color=[catppuccin["peach"], catppuccin["peach"]],
+            other_screen_border=catppuccin["gray0"],
+            other_current_screen_border=catppuccin["white"],
+            this_current_screen_border=catppuccin["white"],
+            this_screen_border=catppuccin["gray0"],
+            inactive=catppuccin["black1"],
+        ),
+        widget.TextBox(
+            text="", padding=0, fontsize=30, foreground=catppuccin["peach"]
+        ),
+        widget.WindowName(fontsize=12, foreground=catppuccin["white"]),
+        widget.TextBox(text="", padding=0, fontsize=30, foreground=catppuccin["teal"]),
+        widget.Volume(
+            fmt="墳 {}",
+            mute_command="amixer -D pulse set Master toggle",
+            background=catppuccin["teal"],
+        ),
+        widget.TextBox(
+            text="",
+            padding=0,
+            fontsize=30,
+            foreground=catppuccin["green"],
+            background=catppuccin["teal"],
+        ),
+        widget.CPU(
+            format=" {load_percent:04}%",
+            mouse_callbacks={"Button1": lazy.spawn("htop")},
+            background=catppuccin["green"],
+        ),
+        widget.TextBox(
+            text="",
+            padding=0,
+            fontsize=30,
+            foreground=catppuccin["yellow"],
+            background=catppuccin["green"],
+        ),
+        widget.CapsNumLockIndicator(fmt=" {}", background=catppuccin["yellow"]),
+        widget.TextBox(
+            text="",
+            padding=0,
+            fontsize=30,
+            foreground=catppuccin["peach"],
+            background=catppuccin["yellow"],
+        ),
+        widget.Clock(format=" %a %d %b %Y, %I:%M %p", background=catppuccin["peach"]),
+        widget.TextBox(
+            text="",
+            padding=0,
+            fontsize=30,
+            foreground=catppuccin["red"],
+            background=catppuccin["peach"],
+        ),
+        widget.Battery(
+            format="{char} {percent:2.0%}",
+            charge_char="",
+            discharge_char="",
+            full_char="",
+            unknown_char="",
+            empty_char="",
+            show_short_text=False,
+            background=catppuccin["red"],
+        ),
+        widget.TextBox(
+            text="",
+            padding=0,
+            fontsize=30,
+            foreground=catppuccin["pink"],
+            background=catppuccin["red"],
+        ),
+        widget.TextBox(
+            text="﫼",
+            mouse_callbacks={
+                "Button1": lazy.spawn("dm-tool lock"),
+                "Button2": lazy.shutdown(),
+            },
+            background=catppuccin["pink"],
+        ),
+        widget.Spacer(length=10, background=catppuccin["pink"]),
+        widget.TextBox(
+            text="",
+            mouse_callbacks={
+                "Button1": lazy.spawan("systemctl suspend"),
+                "Button2": lazy.spawn("systemctl poweroff"),
+            },
+            background=catppuccin["pink"],
+        ),
+        widget.TextBox(
+            text="",
+            padding=0,
+            fontsize=30,
+            foreground=catppuccin["pink"],
+            background="#00000000",
+        ),
+        widget.Spacer(length=3, background="#00000000"),
+    ]
+    if primary:
+        widgets.insert(10, widget.Systray())
+    return widgets
+
+
 screens = [
     Screen(
-        bottom=bar.Bar(
-            [
-                widget.CurrentLayout(),
-                widget.GroupBox(),
-                widget.Prompt(),
-                widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-                # widget.StatusNotifier(),
-                widget.Systray(),
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                widget.QuickExit(),
-            ],
+        top=bar.Bar(
+            get_widgets(primary=True),
             24,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+            background="#00000099",
         ),
+    ),
+    Screen(
+        top=bar.Bar(
+            # Use everything except the systray, which would crash
+            get_widgets(primary=False),
+            24,
+            background="#00000099",
+        )
     ),
 ]
 
 # Drag floating layouts.
 mouse = [
-    Drag([mod], "Button1", lazy.window.set_position_floating(), start=lazy.window.get_position()),
-    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
+    Drag(
+        [mod],
+        "Button1",
+        lazy.window.set_position_floating(),
+        start=lazy.window.get_position(),
+    ),
+    Drag(
+        [mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()
+    ),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
