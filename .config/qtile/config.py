e8b581ceb3efe1cb1973706368c79e26669b8de0
mjschwenne <mjschwenne@gmail.com>
2022-05-06 20:56:57 -0400

    fixed misaligned group icons

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -104,7 +104,7 @@ keys = [
 ]
 
 # WORKSPACES
-groups = [Group(i) for i in [" ", " ", " ", " ", " ", " ", " ", " "]]
+groups = [Group(i) for i in ["", "", "", "", "", "", "", ""]]
 group_hotkeys = "asdfuiop"
 
 for g, k in zip(groups, group_hotkeys):
@@ -115,14 +115,14 @@ for g, k in zip(groups, group_hotkeys):
                 [mod],
                 k,
                 lazy.group[g.name].toscreen(),
-                desc=f"Switch to group {i.name}",
+                desc=f"Switch to group {g.name}",
             ),
             # mod1 + shift + letter of group = switch to & move focused window to group
             Key(
                 [mod, "shift"],
                 k,
                 lazy.window.togroup(g.name, switch_group=True),
-                desc=f"Switch to & move focused window to group {i.name}",
+                desc=f"Switch to & move focused window to group {g.name}",
             ),
             # Or, use below if you prefer not to switch to that group.
             # # mod1 + shift + letter of group = move focused window to group
