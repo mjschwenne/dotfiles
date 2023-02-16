b6717c3f649685987aceb2197361234f88d03118
mjs <mjschwen@gmail.com>
2023-02-16 14:24:34 -0500

    update eject.fish for different drive names

--- .config/bspwm/scripts/eject.fish
+++ .config/bspwm/scripts/eject.fish
@@ -1,8 +1,14 @@
 #! /usr/bin/fish
 
-if findmnt -S /dev/sdb1 > /dev/null
-	notify-send "Ejecting /dev/sdb1..."
-	udisksctl unmount -b /dev/sdb1
+if test $hostname = mjs-surface
+	set eject_dirve '/dev/sda1'
+else if test $hostname = ilsensine
+	set eject_dirve '/dev/sdb1'
 end
 
-udisksctl power-off -b /dev/sdb1
+if findmnt -S $eject_dirve > /dev/null
+	notify-send "Ejecting $eject_dirve..."
+	udisksctl unmount -b $eject_dirve
+end
+
+udisksctl power-off -b $eject_dirve
