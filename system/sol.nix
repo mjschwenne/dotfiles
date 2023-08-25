cb40e2df281b082e0dd54756799a96eb6d599a34
Matt Schwennesen <mjschwenne@gmail.com>
2023-08-25 14:32:27 -0500

    attempting to setup nextcloud

--- system/sol.nix
+++ system/sol.nix
@@ -3,6 +3,7 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 { config
 , pkgs
+, lib
 , foundry
 , ...
 } @ inputs: {
@@ -10,6 +11,7 @@
     # Include the results of the hardware scan.
     ./sol-hardware.nix
     foundry.nixosModules.foundryvtt
+    ./nextcloud.nix
   ];
 
   # Bootloader.
@@ -66,6 +68,8 @@
     wget
     curl
     brightnessctl
+
+    syncthing-discovery
   ];
 
   programs.fish.enable = true;
@@ -82,6 +86,24 @@
 
     virtualHosts."foundry.schwennesen.org".extraConfig = ''
       reverse_proxy localhost:30000
+    '';
+
+    virtualHosts."syncthing.schwennesen.org".extraConfig = ''
+      reverse_proxy localhost:8384
+    '';
+
+    virtualHosts."discovery.schwennesen.org".extraConfig = ''
+      reverse_proxy localhost:8443 {
+        header_up X-Forwarded-For {http.request.remote.host}
+        header_up X-Client-Port {http.request.remote.port}
+        header_up X-Tls-Client-Cert-Der-Base64 {http.request.tls.client.certificate_der_base64}
+      }
+
+      tls {
+        client_auth {
+          mode request
+        }
+      }
     '';
   };
 
@@ -118,6 +140,52 @@
 
   # Let other hosts find this computer on the network
   services.avahi.enable = true;
+
+  services.syncthing = {
+    enable = true;
+    user = "mjs";
+    configDir = "/home/mjs/.syncthing";
+
+    settings = {
+      gui = {
+        user = lib.removeSuffix "\n" (builtins.readFile ./syncthing/user);
+        password = lib.removeSuffix "\n" (builtins.readFile ./syncthing/password);
+        insecureSkipHostcheck = true;
+      };
+
+      devices = {
+        "terra" = {
+          id = "SA2KNKU-LTH27QL-OHZSGVW-HCMWWFP-5RYZEER-K3LQKDM-NGF2PNQ-HRVKSQL";
+        };
+        "luna" = {
+          id = "43RQHNP-QWSOVCU-32G6M5U-4TSIRY6-Y26QJBR-FT4DNK3-QRIJEAC-TIXHGA4";
+        };
+        "phone" = {
+          id = "SVMWORW-JCZ26YN-7P77FJC-YYUNZ46-3PXZZQH-TMZGH5F-LD3TVJ4-XEVQMAE";
+        };
+      };
+
+      folders = {
+        "org" = {
+          path = "/home/mjs/Documents";
+          devices = [ "terra" "luna" ];
+        };
+        "kdb" = {
+          path = "/home/mjs/kdb";
+          devices = [ "terra" "luna" "phone" ];
+        };
+      };
+    };
+  };
+
+  systemd.services.stdiscosrv = {
+    enable = true;
+    description = "Syncthing discovery server";
+    unitConfig.Type = "simple";
+    serviceConfig.ExecStart = ''/run/current-system/sw/bin/stdiscosrv -db-dir="/home/mjs/.syncthing/discovery.db" -http'';
+    wantedBy = [ "multi-user.target" ];
+    after = [ "network.target" ];
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
