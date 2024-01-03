e95ed796e8e5509352ee0348f4e82f42bff44404
Matt Schwennesen <mjschwenne@gmail.com>
2024-01-02 23:22:09 -0600

    January 2024 updates

--- home/applications/wonderdraft/default.nix
+++ home/applications/wonderdraft/default.nix
@@ -1,12 +1,12 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [ (pkgs.callPackage ./wonderdraft.nix { }) ];
+{pkgs, ...}: {
+  home.packages = with pkgs; [(pkgs.callPackage ./wonderdraft.nix {})];
 
   xdg.desktopEntries = {
     wonderdraft = {
       name = "Wounderdraft";
       exec = "wonderdraft %U";
       terminal = false;
-      categories = [ "Graphics" ];
+      categories = ["Graphics"];
       type = "Application";
       icon = "wonderdraft";
     };
