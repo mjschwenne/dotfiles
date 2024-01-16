738d1eac3b34238c5f3cd67e1e460d166769857d
Matt Schwennesen <mjschwenne@gmail.com>
2024-01-16 09:18:22 -0600

    fix screen sharing issues due to xdg-portal mismatch

--- system/graphical.nix
+++ system/graphical.nix
@@ -68,11 +68,12 @@
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    portalPackage = inputs.hyprland.packages."${pkgs.system}".xdg-desktop-portal-hyprland;
   };
 
   xdg.portal = {
     enable = true;
-    # extraPortals = [pkgs.xdg-desktop-portal-hyprland];
+    wlr.enable = true;
   };
 
   # Flatpak
