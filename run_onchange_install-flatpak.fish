63853c7550d4611414663e4fe1cef2482449a928
mjschwenne <mjschwenne@gmail.com>
2024-07-28 21:51:08 -0500

    update README

--- run_onchange_install-flatpak.fish
+++ run_onchange_install-flatpak.fish
@@ -2,7 +2,7 @@
 
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-set flatpaks "com.brave.Browser" "com.github.tchx84.Flatseal" "com.protonvpn.www" "org.mozilla.Thunderbird" "ch.protonmail.protonmail-bridge" "io.github.Qalculate" "com.github.xournalpp.xournalpp" "org.zotero.Zotero" "dev.vencord.Vesktop" "com.valvesoftware.Steam" "net.kuribo64.melonDS" "io.github.Cockatrice.cockatrice"
+set flatpaks "com.brave.Browser" "com.github.tchx84.Flatseal" "com.protonvpn.www" "org.mozilla.Thunderbird" "ch.protonmail.protonmail-bridge" "io.github.Qalculate" "com.github.xournalpp.xournalpp" "org.zotero.Zotero" "dev.vencord.Vesktop" "com.valvesoftware.Steam" "net.kuribo64.melonDS" "io.github.Cockatrice.cockatrice" "com.spotify.Client"
 for f in $flatpaks
     flatpak install -y flathub $f
 end
