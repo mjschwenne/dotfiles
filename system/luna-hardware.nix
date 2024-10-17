ba3935e7ad9a43a5e96a4717d445ff098a07af30
Matt Schwennesen <mjschwenne@gmail.com>
2024-10-17 13:08:16 -0500

    fix audio on luna

--- system/luna-hardware.nix
+++ system/luna-hardware.nix
@@ -16,22 +16,23 @@
   boot.initrd.kernelModules = [];
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
+  boot.blacklistedKernelModules = ["ipu3_imgu"];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/4a8099f8-c679-4fe8-8c94-15949438e2c1";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/4a8099f8-c679-4fe8-8c94-15949438e2c1";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/5A1D-CEA8";
-      fsType = "vfat";
-      options = [ "fmask=0022" "dmask=0022" ];
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/5A1D-CEA8";
+    fsType = "vfat";
+    options = ["fmask=0022" "dmask=0022"];
+  };
 
-  fileSystems."/home" =
-    { device = "/dev/disk/by-uuid/16dacec8-eabb-4e1a-bfbc-10132452608b";
-      fsType = "ext4";
-    };
+  fileSystems."/home" = {
+    device = "/dev/disk/by-uuid/16dacec8-eabb-4e1a-bfbc-10132452608b";
+    fsType = "ext4";
+  };
 
   swapDevices = [
     {device = "/dev/disk/by-uuid/2d9c69bf-12cd-4a00-9f8b-0b480ce0c3ca";}
