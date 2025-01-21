ee9ad9011786ccc82bdac4af26db030e7d179e1b
Matt Schwennesen <mjschwenne@gmail.com>
2025-01-20 19:11:20 -0600

    emacs updates

--- home/editors/default.nix
+++ home/editors/default.nix
@@ -1,3 +1,31 @@
-{ ... }: {
-  imports = [ ./emacs ./nvim ];
+{pkgs, ...}: {
+  imports = [./emacs ./nvim];
+
+  # System-wide checkers and linters shared between nvim and emacs
+  packages = with pkgs; [
+    alejandra
+    proselint
+    statix
+    checkmake
+    google-java-format
+    deadnix
+    mypy
+    black
+    isort
+    shfmt
+    stylua
+  ];
+
+  xdg.configFile."proselint/config.json".text =
+    /*
+    json
+    */
+    ''
+      {
+        "checks": {
+          "annotations.misc": false,
+          "lexical_illusions.misc": false,
+        }
+      }
+    '';
 }
